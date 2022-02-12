78423182121ae9e644dc1811c005216678ce2aec
Jeremy Liberman <mrleebo@msn.com>
2022-02-12 14:35:39 -0600

    Update source code link

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -18,7 +18,7 @@ function Layout(props) {
             <Anchor to="/play">play</Anchor>
             <a
               className="text-teal-900 bg-white hover:bg-teal-100 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              href="https://codesandbox.io/s/wordle-solver-m8vut"
+              href="https://github.com/MrLeebo/wordle-solver"
               target="_blank"
               rel="noopener noreferrer"
             >
